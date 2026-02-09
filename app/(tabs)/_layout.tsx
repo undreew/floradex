de3099815f91c9506676361455d6d96a0669465e
james <jamesdejesus74@gmail.com>
2026-02-09 16:32:58 +0800

    added clerk auth and scanning using camera

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,65 @@
+import { Redirect, Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useAuth } from "@clerk/clerk-expo";
+
+export default function TabLayout() {
+	const colorScheme = useColorScheme();
+	const { isSignedIn } = useAuth();
+
+	// Protect tabs - redirect to login if not signed in
+	if (!isSignedIn) {
+		return <Redirect href="/" />;
+	}
+
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+				headerShown: false,
+				tabBarButton: HapticTab,
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Dashboard",
+					tabBarIcon: ({ color }) => (
+						<IconSymbol size={28} name="house.fill" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="scan"
+				options={{
+					title: "Scan Plants",
+					tabBarIcon: ({ color }) => (
+						<IconSymbol size={28} name="paperplane.fill" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="quiz"
+				options={{
+					title: "Plant Quiz",
+					tabBarIcon: ({ color }) => (
+						<IconSymbol size={28} name="chevron.right" color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="upload"
+				options={{
+					title: "Upload Plant",
+					tabBarIcon: ({ color }) => (
+						<IconSymbol size={28} name="chevron.left" color={color} />
+					),
+				}}
+			/>
+		</Tabs>
+	);
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
 export default function RootLayout() {
-  return <Stack />;
+	const colorScheme = useColorScheme();
+
+	return (
+		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+			<ClerkProvider
+				tokenCache={tokenCache}
+				publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+			>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="index" />
+					<Stack.Screen name="(tabs)" />
+				</Stack>
+				<StatusBar style="auto" />
+			</ClerkProvider>
+		</ThemeProvider>
+	);
 }
